4d28cb86e8291783d9a8520a20f1a8957f3ca94f
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2022-02-16 13:44:54 +0100

    removing inner button shadow on touch/click

--- src/components/WheelButton.tsx
+++ src/components/WheelButton.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 export const WheelButton = ({ spin }: { spin: () => void }) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
   return (
-    <g style={{ cursor: "pointer" }} onClick={spin}>
+    <g
+      style={{ cursor: "pointer" }}
+      onClick={spin}
+      onMouseDown={() => setIsClicked(true)}
+      onMouseUp={() => setIsClicked(false)}
+      onMouseLeave={() => setIsClicked(false)}
+      onTouchStart={() => setIsClicked(true)}
+      onTouchCancel={() => setIsClicked(false)}
+      onTouchEnd={() => setIsClicked(false)}
+    >
       <defs>
         <filter id="innerShadow">
           <feDropShadow dx="0" dy="20" stdDeviation="0" floodOpacity={0.3} />
@@ -10,7 +23,13 @@ export const WheelButton = ({ spin }: { spin: () => void }) => {
         </filter>
       </defs>
       <circle cx="500" cy="500" r="160" fill="#f8f9fa" style={{ filter: "url(#outerShadow)" }} />
-      <circle cx="500" cy="500" r="140" fill="rgb(239, 238, 238)" style={{ filter: "url(#innerShadow)" }} />
+      <circle
+        cx="500"
+        cy="500"
+        r="140"
+        fill="rgb(239, 238, 238)"
+        style={{ filter: isClicked ? "" : "url(#innerShadow)" }}
+      />
       <foreignObject x="360" y="360" width={"280"} height={"280"}>
         <div className="flex items-center justify-center w-full h-full p-4">
           <p className="font-bold font-custom text-gray-600" style={{ fontSize: "calc(24px + 2vmin)" }}>
